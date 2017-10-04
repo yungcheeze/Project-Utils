2008432ef5a3e52231d84bef6400555165c9578c
yungcheeze <ucizim@gmail.com>
2017-10-04 14:02:24 +0200

    add new plotting functions
    - plot custom
    - plot size change
    - plot capacity change
    - print value analysis

--- py/plotter.py
+++ py/plotter.py
@@ -1,33 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from pyparser import get_line_val, get_dict_val
 from misc_funcs import make_format
-# import pprint as pp
-
-print("loading files")
-
-data = np.load("./parsed_filtered_28-09-17_1720.npz")
-
-print(data.files)
-
-log_file_path = "../../results/Mem_Analysis_2/filtered_28-09-17_1720.log"
-# entries = data['entries'].item()
-
-print "files loaded"
+from line_parser import get_line_val, get_dict_val
 
 
-def color_y_axis(ax, color):
-    """Color your axes."""
-    for t in ax.get_yticklabels():
-        t.set_color(color)
-    return None
-
-
-def to_mb(val):
-    val = float(val)
-    return val / 1000000
-
-
+# Main Plotting Functions
 def plot_single_val(stmt_key, conversion_func=str):
     global entries
     global log_file_path
@@ -48,6 +25,73 @@ def plot_timestamp(stmt_key, conversion_func=str):
     for i in entries.get(stmt_key):
         yield conversion_func(get_line_val(log_file_path, i, "timestamp"))
 
+
+def plot_custom(stmt_key, plotting_func):
+    global entries
+    global log_file_path
+    for i in entries.get(stmt_key):
+        val, is_valid = plotting_func(log_file_path, i)
+        if is_valid:
+            yield val
+
+
+def color_y_axis(ax, color):
+    """Color your axes."""
+    for t in ax.get_yticklabels():
+        t.set_color(color)
+    return None
+
+
+# Conversion Functions
+def to_mb(val):
+    val = float(val)
+    return val / 1000000
+
+
+# custom plotting functions
+def plot_size_change(log_file_path, linum):
+    old_size = int(get_dict_val(log_file_path, linum, "old_size"))
+    new_size = int(get_dict_val(log_file_path, linum, "new_size"))
+    change = new_size - old_size
+    isValid = True if change != 0 else False
+    return (change, isValid)
+
+
+def plot_capacity_change(log_file_path, linum):
+    old_capacity = int(get_dict_val(log_file_path, linum, "old_capacity"))
+    new_capacity = int(get_dict_val(log_file_path, linum, "new_capacity"))
+    change = new_capacity - old_capacity
+    isValid = True if change != 0 else False
+    return (change, isValid)
+
+
+def print_value_analysis(func, stat, vals):
+    print "value analysis: " + func + " " + stat
+    zero_count, pos_count, neg_count = 0, 0, 0
+    for v in vals:
+        if v < 0:
+            neg_count += 1
+        elif v == 0:
+            pos_count += 1
+        else:
+            pos_count += 1
+
+    print ("negatives: ", neg_count)
+    print ("positives: ", pos_count)
+    print ("zeros: ", zero_count)
+
+
+
+print("loading files")
+
+data = np.load("./parsed_filtered_28-09-17_1720.npz")
+
+print(data.files)
+
+log_file_path = "../../results/Mem_Analysis_2/filtered_28-09-17_1720.log"
+entries = data['entries'].item()
+
+print "files loaded"
 
 
 print("init plots")
