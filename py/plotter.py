24f181c60ffeaf3a745a545567cd3d9d67cf8d05
Ucizi Mafeni <ucizim@gmail.com>
2017-10-21 10:39:05 +0100

    snapshot of plotter.py

--- py/plotter.py
+++ py/plotter.py
@@ -31,8 +31,7 @@ def plot_custom(stmt_key, plotting_func):
     global log_file_path
     for i in entries.get(stmt_key):
         val, is_valid = plotting_func(log_file_path, i)
-        if is_valid:
-            yield val
+        yield val
 
 
 def color_y_axis(ax, color):
@@ -100,28 +99,48 @@ fig, ax = plt.subplots()
 ax.set_xlabel("time(s)")
 ax.set_ylabel("megabytes")
 
-# x1 = list(plot_timestamp("total_capacity"))
-# y1 = list(plot_single_val("total_capacity", conversion_func=to_mb))
+x1 = list(plot_timestamp("total_capacity"))
+y1 = list(plot_single_val("total_capacity", conversion_func=to_mb))
 h1, = ax.plot(x1, y1, 'r', label="total memory allocated")
 print "plot 1 complete"
 
-# x1_2 = list(plot_timestamp("total_size"))
-# y1_2 = list(plot_single_val("total_size", conversion_func=to_mb))
-h1_2, = ax.plot(x1_2, y1_2, 'g', label="total used memory (out of total allocated)")
+x1_2 = list(plot_timestamp("total_size"))
+y1_2 = list(plot_single_val("total_size", conversion_func=to_mb))
+h1_2, = ax.plot(x1_2, y1_2, 'g--', label="total used memory")
 print "plot 2 complete"
 
 ax2 = ax.twinx()
 ax2.set_ylabel("bytes")
 
-# x2 = list(plot_timestamp("constructor"))
-# y2 = list(plot_dict_val("constructor", "size"))
-h2, = ax2.plot(x2, y2, 'b_', label="memory allocated by call to constructor")
+x2 = list(plot_timestamp("constructor"))
+y2 = list(plot_dict_val("constructor", "size"))
+h2, = ax2.plot(x2, y2, 'bx', label="mem alloc constructor", linewidth=7)
 print "plot 3 complete"
+
+x2_1 = list(plot_timestamp("destructor"))
+y2_1 = list(plot_dict_val("destructor", "mem_freed"))
+h2_1, = ax2.plot(x2_1, y2_1, 'kx', label="mem free destructor", linewidth=7)
+print "plot 4 complete"
+
+x2_2 = list(plot_timestamp("resize"))
+y2_2 = list(plot_custom("resize", plot_size_change))
+h2_2, = ax2.plot(x2_2, y2_2, 'm_', label="resize: size change", linewidth=7)
+print "plot 5 complete"
+
+x2_3 = list(plot_timestamp("resize"))
+y2_3 = list(plot_custom("resize", plot_capacity_change))
+h2_3, = ax2.plot(x2_3, y2_3, 'c_', label="resize: capacity change", linewidth=7)
+print "plot 6 complete"
+
+x2_4 = list(plot_timestamp("shrink_to_fit"))
+y2_4 = list(plot_custom("shrink_to_fit", plot_capacity_change))
+h2_4, = ax2.plot(x2_4, y2_4, 'g_', label="shrink_to_fit: capacity change", linewidth=7)
+print "plot 7 complete"
 
 color_y_axis(ax, 'r')
 color_y_axis(ax2, 'b')
 
 ax2.format_coord = make_format(ax2, ax)
 
-ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., handles=[h1, h1_2, h2])
+ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., handles=[h1, h1_2, h2, h2_1])
 print("finished")
